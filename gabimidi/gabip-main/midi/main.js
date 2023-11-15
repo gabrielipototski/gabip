e00611a036113deadf4c2d67daff65bc12d9bd11
Gabi <116088564+gabrielipototski@users.noreply.github.com>
2023-11-15 14:10:15 -0300

    Add files via upload

---
+++ gabimidi/gabip-main/midi/main.js
@@ -0,0 +1,9 @@
+function tocaSomPom () {                         
+document.querySelector('#som_tecla_pom').play();}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorA11('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom
+
